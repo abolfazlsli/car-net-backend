35d00374775241ebf4ab569fcdd2f9decb9992c9
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-02-20 21:52:38 +0330

    add check user and edit user APIs

--- users/model.py
+++ users/model.py
@@ -1,5 +1,5 @@
 from extensions import db
-
+from datetime import datetime
 class Users (db.Model) :
     __bind_key__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +23,17 @@ class Users (db.Model) :
         self.assetsdir = assetsdir
         
 
+
+class Activity(db.Model):
+    __bind_key__ = "users"
+    id = db.Column(db.Integer, primary_key=True)  
+    userid = db.Column(db.String, nullable=False)  
+    action = db.Column(db.String(100), nullable=False) 
+    date = db.Column(db.DateTime, default=datetime.utcnow) 
+    status = db.Column(db.String(50), nullable=False) 
+    actionid = db.Column(db.String , unique = True)
+    def __init__(self, userid, action, status , actionid):
+        self.userid = userid
+        self.action = action
+        self.status = status
+        self.actionid = actionid
--- users/route.py
+++ users/route.py
@@ -21,9 +21,11 @@ def adduser () :
     userdir = f"./users/assets/{username}_{digitalid}"
     try:
         user = Users(username , name , lastname , phone , password , digitalid ,userdir ,userdir)
+        activity = Activity(digitalid , "ثبت نام" , "done" , generate_random_string())
         token = Tokens(token_digiid , digitalid , set30daysnext() , sendtoday() , request.headers.get('User-Agent')  , "P")
         db.session.add(token)
         db.session.add(user)
+        db.session.add(activity)
         db.session.commit()
         os.mkdir(userdir)
         return {
@@ -44,7 +46,9 @@ def loginuser ():
         token_key = generate_random_string()
         user_key = user.first().digitalid
         token = Tokens(token_key , user_key , set30daysnext() ,sendtoday() , request.headers.get('User-Agent')  , "C")
+        activity = Activity(user_key , "ورود" , "done" , generate_random_string())
         db.session.add(token)
+        db.session.add(activity)
         db.session.commit()
         return {
             "token" : token_key ,
@@ -53,3 +57,45 @@ def loginuser ():
     return {
         "error" : "invalid user"
     } , 500 
+
+
+
+
+@users.post("/me")
+def me () :
+    data = request.form['token']
+    tok = Tokens.query.filter_by(key = data)
+    userinfo = Users.query.filter_by(digitalid = tok.first().user)
+    apidata = {
+        "username" : userinfo.first().username , 
+        "name" : userinfo.first().name , 
+        "lastname" : userinfo.first().lastname, 
+        "phone" : userinfo.first().phone , 
+        "profileurl" : userinfo.first().profileurl , 
+        "assetsdir" : userinfo.first().assetsdir
+    }
+    return apidata
+
+
+@users.post("/edituser") 
+def editUser () :
+    token = request.form.get("token")
+    name = request.form.get("name")
+    lastname = request.form.get("lastname")
+    phone = request.form.get("phone")
+    userid = Tokens.query.filter_by(key = token).first().user
+    user = Users().query.filter_by(digitalid = userid)
+    
+
+@users.post("/check/username")
+def checkuser () :
+    data = request.form.get("username")
+    user = Users.query.filter_by(username = data)
+    if user.count () > 0 : 
+        return {
+            "data" : True
+        }
+    else :
+        return {
+            "data" : False
+        }
