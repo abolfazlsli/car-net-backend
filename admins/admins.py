f40bf231365bfc6501a97c5d41c4c4dfc0df01a3
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-03-10 00:39:45 +0330

    fixed brands

--- admins/admins.py
+++ admins/admins.py
@@ -1,6 +1,8 @@
 from flask import *
 from admins.admins_module import *
 from apiseting.api_seciurety import checksubapp , checkapikey
+from cars.cars_module import Brands , BrandFiles
+from appfunctions import generate_random_string
 admins = Blueprint(name="admins" , import_name=__name__ , url_prefix="/admins")
 
 
@@ -11,6 +13,60 @@ def adminshome () :
     checkapikey(request)
     return {
         "data" : "welcom to admins"
+    }
+
+
+@admins.post("/addbrand")
+def addbrand () :
+    checksubapp("admins")
+    # checkapikey(request)
+    data = request.json
+    print(data)
+    brandname = data.get("brandname")
+    feilds = data.get("feilds")
+    brandid = generate_random_string()
+    for i in feilds:
+        brandfile = BrandFiles(brandid , i.get("filetype") , i.get("filename") , i.get("options"))
+        db.session.add(brandfile)
+    brand = Brands(brandname , brandid)
+    db.session.add(brand)
+    db.session.commit()
+    return {
+        "data" : "brand added successfully"
+    }
+
+@admins.get("/getbrands")
+def getbrands () :
+    checksubapp("admins")
+    brands = Brands.query.all()
+    return {
+        "data" : brands
+    }
+
+@admins.put("/updatebrand")
+def updatebrand () :
+    checksubapp("admins")
+    data = request.json
+    brandid = data.get("brandid")
+    brandname = data.get("brandname")
+    brand = Brands.query.filter_by(brandid = brandid).first()
+    brand.brandname = brandname
+    db.session.commit()
+    return {
+        "data" : "brand updated successfully"
+    }
+
+
+@admins.delete("/deletebrand")
+def deletebrand () :
+    checksubapp("admins")
+    data = request.json
+    brandid = data.get("brandid")
+    brand = Brands.query.filter_by(brandid = brandid).first()
+    db.session.delete(brand)
+    db.session.commit()
+    return {
+        "data" : "brand deleted successfully"
     }
 
 @admins.get("/")
--- cars/cars_module.py
+++ cars/cars_module.py
@@ -2,11 +2,34 @@ from extensions import db
 
 class Cars (db.Model):
     __bind_key__ = "cars"
-    def __init__(self , name):
+    def __init__(self , name, carid , brandid , title , description , price , images_dir , imagecover , status , expire_date , views , created_at , updated_at):
         self.name = name
+        self.carid = carid
+        self.brandid = brandid
+        self.title = title
+        self.description = description
+        self.price = price
+        self.images_dir = images_dir
+        self.imagecover = imagecover
+        self.status = status # pinding , active , inactive , deleted , archive , blocked , expired
+        self.expire_date = expire_date
+        self.views = views
+        self.created_at = created_at
+        self.updated_at = updated_at
     id = db.Column(db.Integer , primary_key = True)
     name = db.Column(db.String)
-
+    carid = db.Column(db.String , nullable = False , unique = True)
+    brandid = db.Column(db.String , nullable = False)
+    title = db.Column(db.String , nullable = False)
+    description = db.Column(db.String)
+    price = db.Column(db.String , nullable = False)
+    images_dir = db.Column(db.String , nullable = False)
+    imagecover = db.Column(db.String , nullable = False)
+    status = db.Column(db.String , nullable = False)
+    expire_date = db.Column(db.String , nullable = False)
+    views = db.Column(db.Integer , nullable = False)
+    created_at = db.Column(db.String , nullable = False)
+    updated_at = db.Column(db.String , nullable = False)
 
 
 class Brands (db.Model):
@@ -17,3 +40,28 @@ class Brands (db.Model):
     id = db.Column(db.Integer , primary_key = True)
     brandid = db.Column(db.String , nullable = False , unique = True)
     brandname = db.Column(db.String , nullable = False)
+
+class BrandFiles (db.Model):
+    __bind_key__ = "cars"
+    def __init__(self , brandid , filetype , filename):
+        self.brandid = brandid
+        self.filetype = filetype
+        self.filename = filename
+    id = db.Column(db.Integer , primary_key = True)
+    brandid = db.Column(db.String , nullable = False)
+    filetype = db.Column(db.String , nullable = False)
+    filename = db.Column(db.String , nullable = False)
+    options = db.Column(db.String , nullable = False)
+
+class FullFiles (db.Model):
+    __bind_key__ = "cars"
+    def __init__(self , carid , filetype , filename , fileidvalue):
+        self.carid = carid
+        self.filetype = filetype
+        self.filename = filename
+        self.fileidvalue = fileidvalue
+    id = db.Column(db.Integer , primary_key = True)
+    carid = db.Column(db.String , nullable = False)
+    filetype = db.Column(db.String , nullable = False)
+    filename = db.Column(db.String , nullable = False)
+    fileidvalue = db.Column(db.String , nullable = False)
