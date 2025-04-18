0d6a7a5cdf88907c8c10b7e79c0b17c9cbe709f7
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-04-19 00:15:20 +0330

    files problems

--- filemanager/module.py
+++ filemanager/module.py
@@ -2,7 +2,7 @@ from extensions import db
 
 class FileManager (db.Model) :
     __bind_key__ = "files"
-    def __init__(self , filename, senderid, fileid , dicription , title , digitaldilename , create_at) :
+    def __init__(self , filename, senderid, fileid , dicription , title , digitaldilename , create_at , strid) :
         self.filename = filename
         self.senderid = senderid
         self.fileid = fileid
@@ -10,6 +10,7 @@ class FileManager (db.Model) :
         self.title = title
         self.digitaldilename = digitaldilename
         self.create_at = create_at
+        self.strid = strid # shopid or user id
     id = db.Column(db.Integer , primary_key = True)
     filename = db.Column(db.String , nullable = False)
     senderid = db.Column(db.String , nullable = False)
@@ -18,3 +19,4 @@ class FileManager (db.Model) :
     title = db.Column(db.String , nullable = False)
     digitaldilename = db.Column(db.String , nullable = False)   # the file name is the real file name that may be persion or unread able type we use this name to save the file and use the file name to read the file 
     create_at = db.Column(db.String , nullable = False)
+    strid = db.Column(db.String , nullable = False)
--- filemanager/route.py
+++ filemanager/route.py
@@ -1,7 +1,7 @@
 from flask import Blueprint , request , send_file
 from filemanager.module import FileManager , db
 from appfunctions import generate_random_string , check_path , makeDir , sendtoday , removefile , getToken
-from sqlalchemy import or_
+from sqlalchemy import or_ , and_
 from tokens.module import Tokens
 from shops.module import Shop
 from http.cookies import SimpleCookie
@@ -47,7 +47,7 @@ def writefileforshop () :
     diginame = f"{fileid}.{file.get("file").filename.split(".")[-1]}"
     user = Tokens.query.filter_by(key = data.get("token"))
     print(user.first().user)
-    filedata = FileManager(file.get("file").filename , user.first().user , fileid , data.get("dicription") ,  file.get("file").filename , diginame , sendtoday())
+    filedata = FileManager(file.get("file").filename , user.first().user , fileid , data.get("dicription") ,  file.get("file").filename , diginame , sendtoday() , data.get("shopid"))
     try:
         if not check_path(f"./filemanager/files/{user.first().user}"):
             makeDir(f"./filemanager/files/{user.first().user}")
@@ -112,7 +112,7 @@ def writefile () :
     file = request.files
     fileid = generate_random_string()
     diginame = f"{fileid}.{file.get("file").content_type}"
-    filedata = FileManager(file.get("file").filename , data.get("usekey") , fileid , data.get("dicription") ,  file.get("file").filename , diginame)
+    filedata = FileManager(file.get("file").filename , data.get("usekey") , fileid , data.get("dicription") ,  file.get("file").filename , diginame , data.get("shopid"))
     if not check_path(f"./filemanager/files/{fileid}_{data.get("usekey")}"):
         makeDir(f"./filemanager/files/{fileid}_{data.get("usekey")}")
     db.session.add(filedata)
@@ -134,15 +134,16 @@ def readcar (carid):
     )
     return ""
 
-@filemanager.get("/read/<userid>/<searchparam>")
-def readfile (searchparam) :
+@filemanager.get("/read/<shopid>/<searchparam>")
+def readfile (searchparam , shopid) :
     print(getToken(request))   
     file = FileManager.query.filter(
-        or_(
-            FileManager.filename == searchparam , 
-            FileManager.digitaldilename == searchparam , 
-            FileManager.title == searchparam
-        )
+            FileManager.strid == shopid,
+            or_(
+                FileManager.filename == searchparam , 
+                FileManager.digitaldilename == searchparam , 
+                FileManager.title == searchparam
+            )
     ).first()
     fileoutput = f"./filemanager/files/{file.senderid}/{file.digitaldilename}"
     return send_file(fileoutput , download_name=file.filename)
